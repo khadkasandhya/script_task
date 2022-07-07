ca8213c97f484237562152db711423d6fd55bb4e
khadkasandhya <46242702+khadkasandhya@users.noreply.github.com>
2022-07-08 04:44:16 +1000

    Add files via upload
    reading csv file

--- user_upload.php
+++ user_upload.php
@@ -10,10 +10,53 @@ if($mysqli === false){
 /* Attempt create database query execution */
 $sql = "CREATE DATABASE demo";
 if($mysqli->query($sql) === true){
+         
     echo "Database created successfully";
-} else{
+} 
+else{
     echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
 }
+$mysqli = new mysqli("localhost", "root", "","demo");
+$query = "CREATE TABLE  users(
+id int,
+name varchar(244),
+surname varchar(244),
+email varchar(244)
+)";
+if ($mysqli->query($query) === true) {
+  echo "Table is successfully created in My_Company database.";
+} else {
+ echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+}
+if (($open = fopen("users.csv", "r")) !== FALSE) 
+  {
+  
+    while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+    {        
+                // Get row data
+                $name = $data[0];
+                $surname = $data[1];
+                $email = $data[2];
+                
+                $query = "INSERT INTO users (name, surname, email) VALUES ('" . $name . "', '" . $surname . "', '" . $email. "')";
+    if ($mysqli->query($query) === true) {
+  echo "Table is inserted.";
+} else {
+ echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+}
+                }
+ 
+            // Close opened CSV file
+    }
+  
+    fclose($open);
+
+ 
+ 
+           
+            // fclose($csvFile);
+         
+    
  
 /* Close connection */
 $mysqli->close();
